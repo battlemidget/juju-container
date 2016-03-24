47de6b77fe8baa10338a036c137543227f231a61
Matt Bruzek <matthew.bruzek@canonical.com>
2016-03-23 22:26:53 -0500

    Removed the juju-core2 stuff, made this script only install juju stable.

--- setup-juju.sh
+++ setup-juju.sh
@@ -4,19 +4,17 @@
 
 set -ex
 
-# Sleep a few seconds to let the networking come up.
+# Sleep a few seconds to let the network come up.
 sleep 3
-apt-get update -qq --fix-missing
+
 # Install tools like add-apt-repository
 apt-get install -qy software-properties-common
-apt-add-repository -y ppa:juju/devel
-# The stable ppa is required for charm-tools
+# The stable ppa is required for charm-tools.
 apt-add-repository -y ppa:juju/stable
 apt-get update -qq --fix-missing
 
-apt-get -qy install juju-core2
-apt-get -qy install byobu vim charm-tools openssh-client
-apt-get -qy install virtualenvwrapper python-dev cython git
+apt-get install -qy juju charm-tools byobu vim tree openssh-client \
+  virtualenvwrapper python-dev cython git
 
 # Ubuntu user exists in the LXC images, no need to create the user.
 
@@ -25,9 +23,7 @@ HOME=/home/ubuntu
 RC=${HOME}/.bashrc
 echo "export JUJU_HOME=${HOME}/.juju" >> $RC
 echo "export JUJU_REPOSITORY=${HOME}" >> $RC
-echo "export PROJECT_HOME=${HOME}" >> $RC
-echo "export JUJU_DATA=${HOME}/.local/share/juju" >> $RC
-
-echo "echo 'welcome to juju 2.0'" >> $RC
+JUJU_VERSION=$(juju version)
+echo "echo 'welcome to $JUJU_VERSION'" >> $RC
 
 chown -R ubuntu:ubuntu ${HOME}
