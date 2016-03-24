ee0f4d4f2755ac01fab59e9a512fc976666aab4c
Matt Bruzek <matthew.bruzek@canonical.com>
2016-03-23 22:24:52 -0500

    Adding the ability to accept/detect codename, and architecture.

--- build.sh
+++ build.sh
@@ -4,32 +4,42 @@
 
 set -ex
 
-if [ -z $JUJU_REPOSITORY ]; then
+if [ -z "$1" ]; then
+  CODENAME=$(lsb_release -cs)
+else
+  CODENAME=$1
+fi
+ARCHITECTURE=$(dpkg --print-architecture)
+
+# Add the linuxcontainers image repository.
+lxc remote add linuxcontainers images.linuxcontainers.org
+
+CONTAINER_NAME=juju-container
+
+# Pull a clean image from the repository.
+lxc launch linuxcontainers:ubuntu/${CODENAME}/${ARCHITECTURE} ${CONTAINER_NAME}
+
+lxc list
+
+if [ -z ${JUJU_REPOSITORY} ]; then
   echo "No JUJU_REPOSITORY found, please enter your charm directory: "
   read JUJU_REPOSITORY
 fi
 
-# Add the linuxcontainers image repository.
-lxc remote add linuxcontainers images.linuxcontainers.org
-# Pull a clean image from the repository.
-lxc launch linuxcontainers:ubuntu/xenial/amd64 juju-container
-
-lxc list
-
 # Push the setup file and run it, remove the file when complete.
-lxc file push setup-juju.sh juju-container/home/ubuntu/
-lxc exec juju-container -- /home/ubuntu/setup-juju.sh
-lxc exec juju-container -- rm /home/ubuntu/setup-juju.sh
+lxc file push setup-juju2.sh ${CONTAINER_NAME}/home/ubuntu/
+lxc exec ${CONTAINER_NAME} -- /home/ubuntu/setup-juju2.sh
+lxc exec ${CONTAINER_NAME} -- rm /home/ubuntu/setup-juju2.sh
 
 # Mount the devices.
-lxc config device add juju-container trusty disk path=/home/ubuntu/trusty \
+lxc config device add ${CONTAINER_NAME} trusty disk path=/home/ubuntu/trusty \
 source=$JUJU_REPOSITORY/trusty
-lxc config device add juju-container xenial disk path=/home/ubuntu/xenial \
+lxc config device add ${CONTAINER_NAME} xenial disk path=/home/ubuntu/xenial \
 source=$JUJU_REPOSITORY/xenial
-lxc config device add juju-container juju-home disk \
+lxc config device add ${CONTAINER_NAME} juju-home disk \
 path=/home/ubuntu/.local/share/juju source=$HOME/.local/share/juju
 
 # Push the cleanup file and run it, remove the file when complete.
-lxc file push cleanup.sh juju-container/home/ubuntu/
-lxc exec juju-container -- /home/ubuntu/cleanup.sh
-lxc exec juju-container -- rm /home/ubuntu/cleanup.sh
+lxc file push cleanup.sh ${CONTAINER_NAME}/home/ubuntu/
+lxc exec ${CONTAINER_NAME} -- /home/ubuntu/cleanup.sh
+lxc exec ${CONTAINER_NAME} -- rm /home/ubuntu/cleanup.sh
